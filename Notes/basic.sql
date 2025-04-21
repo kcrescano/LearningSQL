fddff5d23f6cd3ee183ffde3302dbc5352d1c0bc
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-21 13:56:57 -0400

    Update basic.sql

--- Notes/basic.sql
+++ Notes/basic.sql
@@ -1,10 +1,33 @@
+-- Create database and table
 CREATE DATABASE students;
-CREATE TABLE students_info ( 
-    student_id INT, 
-    name VARCHAR(30), 
-    surname VARCHAR(30), 
+CREATE TABLE students_info (
+    student_id INT,
+    name VARCHAR(30),
+    surname VARCHAR(30),
     age INT
 );
 
 DROP TABLE students_info;
-DROP DATABASE students; 
+DROP DATABASE students;
+
+-- Select literals
+SELECT 'Hello, World!';
+SELECT 2.3*4+5;
+
+SELECT 'Bob' AS name, 160 AS "height in centimeters", 160*0.393701 AS "height in inches";
+SELECT
+    "John" as first_name,
+    "Jeffery" as second_name,
+    20000 as salary,
+    "IT" as department
+;
+
+-- Select from
+SELECT
+    student_id AS place,
+    name as first_name,
+    surname as last_name,
+    age
+FROM
+    students_info
+;
